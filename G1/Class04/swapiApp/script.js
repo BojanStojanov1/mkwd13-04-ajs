4945b2a92d50a26ba5db59bf64b490b0a19c17d8
trajanstevkovski <trajanstevkovski@gmail.com>
2025-01-20 19:31:57 +0100

    Class04 swapi example

---
+++ G1/Class04/swapiApp/script.js
@@ -0,0 +1,65 @@
+document.getElementById('get-sw-people')
+    .addEventListener('click', function() {
+        getSwapiPeoples('https://swapi.dev/api/people');
+    });
+
+document.getElementById('prev')
+    .addEventListener('click', function(event) {
+        let url = event.target.value;
+        getSwapiPeoples(url);
+    });
+
+document.getElementById('next')
+    .addEventListener('click', function(event) {
+        let url = event.target.value;
+        getSwapiPeoples(url);
+    });
+
+function showSwapiPeople(people) {
+    let html = '<ol>';
+    for(let person of people) {
+        let li = `
+            <li>
+                ${person.name} has appeared in ${person.films.length} films
+            </li>
+        `;
+        html += li;
+    }
+    html += '</ol>';
+    document.getElementById('content').innerHTML = html;
+}
+
+function showPrevNextPageBtn(data) {
+    let prevBtn = document.getElementById('prev');
+    prevBtn.style.display = 'block';
+    let nextBtn = document.getElementById('next');
+    nextBtn.style.display = 'block';
+    
+    if (data.previous) {
+        // disable btn
+        prevBtn.disabled = false;
+        prevBtn.value = data.previous;
+    } else {
+        // unable
+        prevBtn.disabled = true;
+    }
+
+    if (data.next) {
+        nextBtn.disabled = false;
+        nextBtn.value = data.next;
+    } else {
+        nextBtn.disabled = true;
+    }
+}
+
+function getSwapiPeoples(url) {
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        showSwapiPeople(data.results);
+        showPrevNextPageBtn(data);
+    });
+}
