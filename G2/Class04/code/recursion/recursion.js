2d3dda38e88ab35b646d02458f86c4eeda8378f7
Ilija Mitev <ilija_mitev_mk@yahoo.com>
2025-01-23 20:44:53 +0100

    Class 04 finished

--- G2/Class04/code/recursion/recursion.js
+++ G2/Class04/code/recursion/recursion.js
@@ -1,9 +1,17 @@
 console.log("===================== RECURSION =====================");
+// Recursion is a technique where a function calls itself
+
+// Use cases: traversing data structures, solving mathematical problems, etc.
+
+// IMPORTANT !!!
+// Always define a base case to stop the recursion
+// Without it, you risk creating infinite loops and stack overflows
 
 // ===> Example: Factoriel
 // 5! = 5 * 4 * 3 * 2 * 1 
 function factoriel(number) {
     debugger;
+    // Exit case !
     if (number <= 1) {
         return 1;
     }
@@ -17,8 +25,10 @@ console.log(factoriel(5))
 // 5) 1 => 1
 // Result => 120
 
+
 // ===> Example: Sum to Number
 function sumToNumber(number) {
+    // Exit case !
     if (number === 0) {
         return 0;
     }
@@ -28,9 +38,11 @@ console.log(sumToNumber(5));
 
 
 // ===> Example: Fibonacci
-// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+// The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, starting from 0 and 1
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
 
 function fibonacci(n) {
+    // Exit case !
     if (n <= 1) {
         return n;
     }
@@ -38,5 +50,7 @@ function fibonacci(n) {
 }
 console.log("Fibonacci of 5 is ", fibonacci(5));
 console.log("Fibonacci of 25 is ", fibonacci(25));
-console.log("Fibonacci of 25 is ", fibonacci(50));
+console.log("Fibonacci of 50 is ", fibonacci(50)); // takes a while...
+
+// Note: For large Fibonacci numbers, recursion can be inefficient due to repeated calculations
 
