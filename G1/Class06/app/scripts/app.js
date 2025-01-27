97d6174752f63e08c9d4a5578218e3827a88b91b
trajanstevkovski <trajanstevkovski@gmail.com>
2025-01-27 19:34:47 +0100

    Class06 workshop

--- G1/Class06/app/scripts/app.js
+++ G1/Class06/app/scripts/app.js
@@ -1,6 +1,9 @@
 const urls = {
-    category: 'https://fakestoreapi.com/products/categories'
+    category: 'https://fakestoreapi.com/products/categories',
+    allProducts: 'https://fakestoreapi.com/products'
 };
+
+let categoryHeader = document.getElementById('category-title');
 
 // get all categories
 
@@ -13,12 +16,22 @@ function getAllCategories() {
         });
 }
 
+function getAllProducts() {
+    fetch(urls.allProducts)
+        .then(res=>res.json())
+        .then(products => {
+            showProducts(products);
+        });
+}
+
+getAllProducts();
+
 function showCategoriesDropDown(data) {
     let btn = `<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Categories</button>`;
     let ul = '<ul class="dropdown-menu">';
     for(let category of data) {
-        let li = `<li><button class="dropdown-item" value="${category}">${category}</button></li>`
+        let li = `<li><button name='category' class="dropdown-item" value="${category}">${category}</button></li>`
         ul += li;
     }
     ul += '</ul>';
@@ -29,4 +42,16 @@ function showCategoriesDropDown(data) {
         .innerHTML = html;
 }
 
+function showProducts(products) {
+
+}
+
 getAllCategories();
+
+document.getElementById('category-filter')
+    .addEventListener('click', (e) => {
+        if(e.target.tagName === 'BUTTON' && e.target.name === 'category') {
+            categoryHeader.innerText = e.target.value;
+        }
+    });
+
