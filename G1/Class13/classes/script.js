9cf6d6c5f856cfa5aef339b185eb3dd2f4490e99
trajanstevkovski <trajanstevkovski@gmail.com>
2025-02-14 19:37:13 +0100

    Class13 classes

--- G1/Class13/classes/script.js
+++ G1/Class13/classes/script.js
@@ -90,6 +90,113 @@ class BookService extends BaseService {
     //....
 }
 
-let bookService = new BookService();
-bookService.showNames(['Trajan', 'Filip']);
+// let bookService = new BookService();
+// bookService.showNames(['Trajan', 'Filip']);
 
+// Static methods
+
+class ElectricVehicle extends Car {
+    constructor(id, name, batch, price, wheels, doors, ac, owner) {
+        super(id, name, batch, price, wheels, doors, ac);
+        this.owner = owner;
+    }
+
+    static addAC(car) {
+        if(!car.ac) {
+            car.ac = true;
+            car.price += 400;
+            console.log(`The car has AC now and it costs ${car.price}`);
+        } else {
+            console.log('The car already has AC');
+        }
+    }
+}
+
+// let electricVehicle = new ElectricVehicle(1, 'Yugo', 123, 500, 4, 5, false, null);
+// console.log(electricVehicle); 
+// electricVehicle.addAC(car);
+
+// ElectricVehicle.addAC(electricVehicle);
+
+class Helpers {
+    static printArray(arr) {
+        arr.forEach(x => {
+            console.log(x);
+        });
+    }
+
+    static printString(str) {
+        console.log(str);
+    }
+
+    static countAllVoewlsInString(str) {
+        let strArr = str.split('');
+        let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        return strArr.filter(char => vowels.includes(char.toLowerCase())).length;
+    }
+}
+
+// Helpers.printArray([1, 2, 3, 4, 5]);
+// Helpers.printString('Trajan');
+// console.log(Helpers.countAllVoewlsInString('aeiouasdsdfsasdh 89asdoasdpioasdiapo'));
+
+
+function sum() {
+    return Helpers.countAllVoewlsInString('asdasdasdasdasdasd') + 10;
+}
+
+// let electricVehicle = new ElectricVehicle();
+// electricVehicle.printVehicle();
+// ElectricVehicle.addAC();
+
+
+class Airplane {
+    constructor(id, name, color, seats){
+        this.seats = seats;
+        this.id = id;
+        this.name = name; 
+        this.color = color;
+    }
+
+    get color(){
+        return this._color;
+    }
+
+    set color(value) {
+        this._color = value;
+    }
+
+    get name() {
+        console.log('We are getting the name of the plane. Please wait...');
+        return 'This plane name is ' + this._name; 
+    }
+
+    set name(value) {
+        console.log('We are setting the name of the plane. Please wait...');
+        value.length > 1 ? this._name = value : this._name = 'Boing';
+    }
+}
+
+// let plane = new Airplane(1, 'Trajan', 'white', 230);
+// console.log(plane);
+
+// let plane1 = new Airplane(1, '', 'white', 230);
+// console.log(plane1);
+
+// console.log('CALLING GET');
+// console.log(plane.name); 
+// console.log('CALLING SET');
+// plane.name = 'Airbus';
+
+
+console.log('==========Checking type of Object============');
+let electric = new ElectricVehicle(1, 'Yugo', 123, 400, 4, 5, false, null);
+
+let isElectricCar = electric instanceof ElectricVehicle;
+// console.log(isElectricCar);
+
+let isElectri = electric instanceof Helpers;
+// console.log(isElectri);
+
+console.log(electric instanceof Car);
