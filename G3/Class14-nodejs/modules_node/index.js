d4c9b383cccb3e5852753240141890400b0be011
anetastankovska <anetastankovskaane@gmail.com>
2025-02-17 21:04:36 +0100

    add class code

--- G3/Class14-nodejs/modules_node/index.js
+++ G3/Class14-nodejs/modules_node/index.js
@@ -1,5 +1,21 @@
+// When we run a JavaScript file in Node, Node does not (by default) treat .js files as ES modules.
+// We need either a package.json with "type": "module", or the .mjs file extension, to let Node know to load the file as an ES module.
 let colors = require("colors");
+
 console.log("Hello world".blue);
-console.log("Hello world".underline.red);
-console.log("Hello world".green);
-console.log("Hello world".yellow);
+let getRandomNumber = () => {
+  return Math.floor(Math.random() * 100);
+};
+
+let displayNumber = (number) => {
+  if (number % 2 === 0) {
+    console.log(`The number ${number} is even`.green);
+  } else {
+    console.log(`The number ${number} is odd`.red);
+  }
+};
+
+let randomNumber = getRandomNumber();
+// console.log("Added new text!");
+
+displayNumber(randomNumber);
--- G3/Class14-nodejs/modules_node_basic_example/index.js
+++ G3/Class14-nodejs/modules_node_basic_example/index.js
@@ -1,32 +1,45 @@
 const sayHello = (name) => {
-  console.log(`Hello ${name}`);
+  console.log(`Hello ${name}!`);
 };
+
+// console.log and setTimeout are both functions that are part of the global objects and depending on the environment where they are execited, a prefix (window/global) is added
 sayHello("John");
-
-// will throw an error because window object doesn't exist in node runtime environment
-// console.log(window);
-
 // The Node.js Global Object is a built-in object that provides global variables and functions accessible throughout the Node.js runtime environment.
 // It is analogous to the window object in browsers but tailored for server-side JavaScript.
 // These are built-in objects that are part of the JavaScript and can be used directly in the application without importing any particular module.
-// console.log(global);
+// console.log("GLOBAL:", global);
 
 // The “process” object use to get current Node.js process details & also give control over that process.
+// console.log("PROCESS:", process);
+
+console.log("MODULE", module);
+// console.log(window); // will throw an error because there is no window object in node
+
+// The setTimeout function will not execute if I uncomment the last line
+setTimeout(() => {
+  console.log("Delayed for 1 second.");
+}, "1000");
+
+// The process.argv array contains the command-line arguments.
+// The first element is the path to Node.js, the second element is the path to the script being run, and subsequent elements are the command-line arguments.
 // console.log(process);
 
-// console.log(module);
-// console.log(process.argv);
+// The process.argv in node is used to access and interact with the command-line arguments.
+// It is an array that contains the command line arguments and helps user to interact node app using CLI.
+// process.argv[0]: is the process execution path
+// process.argv[1]: is the path for the js file.
+// process.argv[2] and more: other items in the array are the additional arguments passed by the user.
+console.log(process.argv);
 
-setTimeout(() => {
-  console.log("Delayed for 1 second");
-}, 1000);
-
+// This is asynchronous non-blocking operation
 process.stdout.write("Enter something: ");
 
+// Process (not winddow/DOM event)
+// Node.js has different events than the DOM
 process.stdin.on("data", (input) => {
   const userInput = input.toString().trim();
   console.log(`You entered: ${userInput}`);
-  //   process.exit();
+  process.exit();
 });
 
-process.exit();
+// process.exit(); // it wont wait us to enter something because process.stdin.on callback function is asynchronous and non blocking
