86067313d897ce53f3141e9bd1cf5459f4e4a013
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-02-22 09:28:32 +0100

    ...

--- G2/Class15-Workshop/code/storage-demo/script.js
+++ G2/Class15-Workshop/code/storage-demo/script.js
@@ -1,5 +1,10 @@
 console.log("============== Local Storage ==============");
-
+// *localStorage* provides persistent storage in the browser (~5MB)
+// Data is stored with no expiration time and remains available even after the browser is closed or the user navigates away from the page
+// Usecases:
+// => saving user preferences
+// => tracking user/app state
+// => storing authentication info etc...
 
 let user = {
     firstName: "Bob",
@@ -9,6 +14,7 @@ let user = {
 
 // ===> Storing Data
 localStorage.setItem("john", "John Doe");
+// NOTE: When working with object, you must first serialize them (transfer them into JSON format)
 localStorage.setItem("currentUser", JSON.stringify(user));
 
 // ===> Retrieving Data
@@ -29,8 +35,9 @@ localStorage.removeItem("john");
 
 
 console.log("============== Optional Chaining Operator '?.' ==============");
+// => it allows you to safely access deeply nested object properties without causing an error if a property is null or undefined
 
 let userObj;
-// console.log(userObj.name); // ERROR
-console.log(userObj?.name);
+// console.log(userObj.name); // THROWS ERROR
+console.log(userObj?.name);  // NO ERROR, logs undefined
 
